c88323232f1c4c7e13894dc9b6a5c6897632b3e1
beihui <beihui@itcast.cn>
2020-08-31 19:29:33 +0800

    默认的

---
+++ day09_GitTest/src/main/java/com/beihui/domain/User.java
@@ -0,0 +1,42 @@
+package com.beihui.domain;
+
+/**
+ * @author 北慧
+ * @date 2020/8/31 19:25
+ */
+public class User {
+    private String name;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
